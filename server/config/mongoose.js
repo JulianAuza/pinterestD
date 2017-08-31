931c9f30ac911dbe89b5a0066976fc97d16920e7
JulianAuza <julianauza@gmail.com>
2017-08-31 16:48:41 -0500

    mLab site link

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://heroku_jv9xgllr:347ci9oj5jp9mt45daps93fa7r@ds111204.mlab.com:11204/heroku_jv9xgllr');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds153501.mlab.com:53501/pinterestclonejulianauza');
 var models_path = path.join(__dirname, './../models');
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') >= 0) {
